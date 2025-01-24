4ba95ec5bcceb699c28ce954a8472bc7a1f78c68
Californien <marlin.lefler29@gmail.com>
2025-01-25 00:06:07 +0100

    changed "Test"-test

--- nuxt/src/tests/Test.vue.test.ts
+++ nuxt/src/tests/Test.vue.test.ts
@@ -1,10 +1,7 @@
-import { mountSuspended } from '@nuxt/test-utils/runtime';
 import { it, expect, describe } from 'vitest';
-import ExampleComp from '../vue/components/ExampleComp.vue';
 
 describe('Example', () => {
-	it('can mount the component', async () => {
-		const cmp = await mountSuspended(ExampleComp);
-		expect(cmp.html()).toContain('<p>Example component</p>');
+	it('correct addition result', async () => {
+		expect(5 + 5).toBe(10);
 	});
 });
