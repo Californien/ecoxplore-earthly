ee821c865858d9a3f904d546b24be0c0f48e4435
Californien <marlin.lefler29@gmail.com>
2025-03-17 19:51:01 +0100

    refactor header component and add gradient divider

--- nuxt/layouts/default.vue
+++ nuxt/layouts/default.vue
@@ -1,6 +1,16 @@
 <template>
 	<div>
-		<Header />
+		<Header class="header" />
 		<slot />
 	</div>
 </template>
+
+<style lang="scss">
+	.header {
+		position: fixed;
+		bottom: 0;
+		left: 0;
+		width: 100%;
+		z-index: 1000;
+	}
+</style>
